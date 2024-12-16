c926fe3aec02e21c257d38cabd74a86676c0854b
ultron <103036831+vj2303@users.noreply.github.com>
2024-12-16 13:55:03 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,14 +13,14 @@ config.autoAddCss = false; // Prevent Font Awesome from adding its CSS automatic
 
 export const metadata = {
   metadataBase: new URL('https://www.luzo.app'),
-  title: 'LUZO | Avail Offers At Premium Salons and Spas',
+  title: 'LUZO | Avail Offers At Premium Salons, Spas & Clinics',
   description: 'Best salons and spas • Trusted by 25,000+ customers • 40% OFF on your first appointment • Free booking • No cancellation fees • No pre-payment required',
   // other: {
   //   "facebook-domain-verification": "8pedts2d6xzp7soentdj3z6rlyep0z",
   // },
   openGraph: {
     // images: [posts.image,],
-    title: `LUZO | Avail Offers At Premium Salons and Spas`,
+    title: `LUZO | Avail Offers At Premium Salons, Spas & Clinics`,
     description: `Best salons and spas • Trusted by 25,000+ customers • 40% OFF on your first appointment • Free booking • No cancellation fees • No pre-payment required`,
     url: `https://luzo.app/`,
   },
